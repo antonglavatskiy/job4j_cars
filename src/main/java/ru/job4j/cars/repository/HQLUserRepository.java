d26690e6c856344a8466f32db04615afdf8415ca
antonglavatskiy <glavazky@yandex.ru>
2023-06-13 10:07:14 +0500

    2. HQL [#6874 #422564]

--- src/main/java/ru/job4j/cars/repository/HQLUserRepository.java
+++ src/main/java/ru/job4j/cars/repository/HQLUserRepository.java
@@ -2,14 +2,20 @@ package ru.job4j.cars.repository;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.cars.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 
 public class HQLUserRepository implements UserRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HQLUserRepository.class.getName());
 
     private final SessionFactory sf;
 
@@ -19,64 +25,95 @@ public class HQLUserRepository implements UserRepository {
 
     @Override
     public User create(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
             session.save(user);
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователь не сохранен", e);
         }
         return user;
     }
 
     @Override
     public void update(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
             session.createQuery(
                             "UPDATE User u SET u.login = :fLogin, u.password = :fPassword WHERE u.id = :fId")
                     .setParameter("fLogin", user.getLogin())
                     .setParameter("fPassword", user.getPassword())
                     .setParameter("fId", user.getId())
                     .executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователь не обновлен", e);
         }
     }
 
     @Override
     public void delete(int id) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
             session.createQuery(
                             "DELETE User u WHERE u.id = :fId")
                     .setParameter("fId", id)
                     .executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователь не удален", e);
         }
     }
 
     @Override
     public List<User> findAllOrderById() {
-        Session session = sf.openSession();
-        Query<User> query = session.createQuery(
-                "from User u ORDER BY u.id", User.class);
-        return query.list();
+        Transaction transaction = null;
+        List<User> rsl = new ArrayList<>();
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User u ORDER BY u.id", User.class);
+            rsl = query.list();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователи не найдены", e);
+        }
+        return rsl;
     }
 
     @Override
     public Optional<User> findById(int id) {
-        Session session = sf.openSession();
-        Query<User> query = session.createQuery(
-                "from User u WHERE u.id = :fId", User.class);
-        query.setParameter("fId", id);
-        return Optional.ofNullable(query.uniqueResult());
+        Transaction transaction = null;
+        Optional<User> rsl = Optional.empty();
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User u WHERE u.id = :fId", User.class)
+                    .setParameter("fId", id);
+            rsl = query.uniqueResultOptional();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователь не найден", e);
+        }
+        return rsl;
     }
 
     @Override
@@ -84,19 +121,41 @@ public class HQLUserRepository implements UserRepository {
         StringBuilder find = new StringBuilder("%")
                 .append(key)
                 .append("%");
-        Session session = sf.openSession();
-        Query<User> query = session.createQuery(
-                "from User u WHERE u.login LIKE :fKey", User.class);
-        query.setParameter("fKey", find.toString());
-        return query.list();
+        Transaction transaction = null;
+        List<User> rsl = new ArrayList<>();
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User u WHERE u.login LIKE :fKey", User.class)
+                    .setParameter("fKey", find.toString());
+            rsl = query.list();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователи не найдены", e);
+        }
+        return rsl;
     }
 
     @Override
     public Optional<User> findByLogin(String login) {
-        Session session = sf.openSession();
-        Query<User> query = session.createQuery(
-                "from User u WHERE u.login = :fLogin", User.class);
-        query.setParameter("fLogin", login);
-        return Optional.ofNullable(query.uniqueResult());
+        Transaction transaction = null;
+        Optional<User> rsl = Optional.empty();
+        try (Session session = sf.openSession()) {
+            transaction = session.beginTransaction();
+            Query<User> query = session.createQuery(
+                    "from User u WHERE u.login = :fLogin", User.class)
+                    .setParameter("fLogin", login);
+            rsl = query.uniqueResultOptional();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error("Пользователь не найден", e);
+        }
+        return rsl;
     }
 }
