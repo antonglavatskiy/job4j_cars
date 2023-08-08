e911bcfe4a5c248d9dab45076e36475cdcd5297a
antonglavatskiy <glavazky@yandex.ru>
2023-08-08 07:12:51 +0500

    6. Модели и связи. Машины и владельцы [#4744 #434716]

--- src/main/java/ru/job4j/cars/model/Car.java
+++ src/main/java/ru/job4j/cars/model/Car.java
@@ -6,8 +6,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 @AllArgsConstructor
@@ -25,11 +26,7 @@ public class Car {
     @JoinColumn(name = "engine_id", foreignKey = @ForeignKey(name = "ENGINE_ID_FK"))
     private Engine engine;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "history_owner",
-            joinColumns = {
-            @JoinColumn(name = "car_id", nullable = false, updatable = false)},
-            inverseJoinColumns = {
-                    @JoinColumn(name = "owner_id", nullable = false, updatable = false)})
-    private Set<Owner> owners = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private List<OwnerHistory> owners = new ArrayList<>();
 }
